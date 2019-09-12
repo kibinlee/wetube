161c9f757e41c9f78e7e514d59197f1a7b9505e5
John Lee <faker@Johns-MacBook-Pro.local>
2019-09-12 13:17:01 +0900

    First Router

--- app.js
+++ app.js
@@ -3,22 +3,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-// const express = require("express");
+import { userRouter } from "./router";
+
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
-
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
@@ -26,12 +17,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-const middleware = (req, res, next) => {
-  res.send("not happening");
-};
-
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
