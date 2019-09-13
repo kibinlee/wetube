93a6f885ab5f26ad8683d884a52d9358161eed60
John Lee <faker@Johns-MacBook-Pro.local>
2019-09-13 14:44:59 +0900

    MongoDB set

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # WeTube
 
 Cloning Youtube with vanilla and NodeJS
+
+- [ ] Home
+- [X] Join
+- [X] Login
+- [x] Search
+- [ ] UserDetail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,24 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { login, logout, join } from "../controllers/userController";
+import {
+  getLogin,
+  getJoin,
+  logout,
+  postLogin,
+  postJoin
+} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
-globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
+globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
