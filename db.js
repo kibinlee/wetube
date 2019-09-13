971b9c8726bf043d861fae07656bd46ce99af10b
John Lee <faker@Johns-MacBook-Pro.local>
2019-09-13 14:57:08 +0900

    Video Model

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅Connected to DB");
+const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = () => console.log(`❌Error on DB Connection Error`);
 
 db.once("open", handleOpen);
