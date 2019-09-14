21c97da122c8382ccbd3af312cdf3f48989e32c5
John Lee <faker@Johns-MacBook-Pro.local>
2019-09-14 13:42:15 +0900

    passport

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+
+import "./passport";
 
 const app = express();
 app.use(helmet());
@@ -21,6 +24,9 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,10 @@
+import passport from "passport";
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -11,6 +13,17 @@ export const postJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
   } else {
     // To Do: Register User
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      // console.log(`Name: ${name} Email: ${email}`);
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+    }
     // To Do: Log user in
     res.redirect(routes.home);
   }
@@ -18,9 +31,11 @@ export const postJoin = (req, res) => {
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Log In" });
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
 
 export const logout = (req, res) => {
   // To Do: Process Log Out
